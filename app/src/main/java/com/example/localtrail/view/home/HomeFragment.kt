ace18d13c89678b195cf5f5665b4def7d5663930
Derek <d329chen@uwaterloo.ca>
2025-07-15 19:30:40 -0400

    pretty up the home page

--- app/src/main/java/com/example/localtrail/view/home/HomeFragment.kt
+++ app/src/main/java/com/example/localtrail/view/home/HomeFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -37,11 +36,6 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
 
         binding.buttonInitiateTrail.setOnClickListener {
             showCreateTrailDialog()
